fb0634e8bd308010451d2aa0d6690f62c1f615f6
Kaushal Raj Kafle <kaushalkafle@gmail.com>
2026-01-31 22:40:16 +0545

    Updated Cache assignment

--- R cache assignment.R
+++ R cache assignment.R
@@ -1,15 +1,36 @@
-makeCacheVector<- function (x=numeric()){
-    cached_mean<-NULL
+# PART 1: Where data is stored
+makeVector <- function(x = c()) {
+    m <- NULL  # Empty cache
     
-    set <-function (y){
-      x<<-y
-      cached_mean<<-NULL #
+    set <- function(y) {
+        x <<- y
+        m <<- NULL  # Clear existing cache
     }
+    
+    get <- function() x
+    
+    setmean <- function(mean_value) {
+        m <<- mean_value  # Just stores a number
+    }
+    
+    getmean <- function() m
+    
+    list(set = set, get = get,
+         setmean = setmean, getmean = getmean)
+}
 
-get<-function ()x
-setmean <- function (mean_value,cached_mean <<-mean_value
-  getmean <- function () cached_mean 
-
-list (set=set, get=get, setmean=setmean, getmean=getmean)
+cachemean <- function(special_vector) {
+    # Check if we already calculated
+    m <- special_vector$getmean()
+    
+    if (!is.null(m)) {
+        message("Using cached mean")
+        return(m)
+    }
+    
+       data <- special_vector$get()      # Get the numbers
+    m <- mean(data)                   # calculation of the mean of the data
+    special_vector$setmean(m)         # Save the result
+    return(m)                         # Return the mean
 }
 
